6e1ad40731169f7c7817378bf9b9c9230414d28d
VirtualZer0 <virtualos@list.ru>
2021-03-23 22:34:51 +0300

    Add links to download, blur instructuons

--- src/screens/Download.vue
+++ src/screens/Download.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="downloadScreen">
     <h1>{{"download" | t($store.state.locale)}}</h1>
-    
+
     <div class="panel-container">
-      <div class="panel hoverable">
+      <a class="panel hoverable" href="https://steamcommunity.com/sharedfiles/filedetails/?id=2433361645" target="_blank">
         <h2>Steam</h2>
         <p>{{"useSteam" | t($store.state.locale)}}</p>
-      </div>
+        <div>{{"download" | t($store.state.locale)}}</div>
+      </a>
 
-      <div class="panel hoverable">
+      <a class="panel hoverable" href="https://moddingofisaac.com/mod/3649/isaac-on-twitch-reloaded" target="_blank">
         <h2>Modding Of Isaac</h2>
         <p>{{"useMOI" | t($store.state.locale)}}</p>
-      </div>
+        <div>{{"download" | t($store.state.locale)}}</div>
+      </a>
 
-      <div class="panel hoverable">
+      <a class="panel hoverable" href="https://github.com/VirtualZer0/IsaacOnTwitchReloaded/archive/refs/heads/master.zip" target="_blank">
         <h2>{{"direct" | t($store.state.locale)}}</h2>
         <p>{{"useDirect" | t($store.state.locale)}}</p>
-      </div>
+        <div>{{"download" | t($store.state.locale)}}</div>
+      </a>
 
     </div>
 
